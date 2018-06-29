83ac20541d321ebf55f8cd6466353bd92d3de5cd
darl4real <darl4real@hotmail.com>
2018-06-29 15:37:08 +0200

    controllers/registerController updated

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,112 +1,56 @@
 const Controller = require('./controller');
-
-const UserModel = require('../models/users');
-class registerController extends Controller
-
-{
-
-    constructor(req, res, next)
-
-    {
-
-        super(req, res, next)
-
+const UserModel = require('../models/userModel');
+const IdentService = require('../service/identService');
+const RegisterService = require('../service/secureService');
+const EmailService = require('../service/emailService');
+class registerController extends Controller {
+    constructor(req, res, next) {
+        super(req, res, next);
     }
-    login()
 
-    {
-
-        let username = this.req.body.uname;
-
-        let pass = this.req.body.psw;
-
-        console.log(typeof(pass));
-
-        let userModel = new UserModel();
-
-        userModel.findUser(username, (info) => {
-            if (info.length === 0) {
-
-                this.req.flash('info', 'El usuario no existe');
-
-                this.index();
-
-            } else {
-                console.log(JSON.stringify(info));
-                if (pass === info[0].password) {
-
-                    this.index();
-                } else {
-
-                    this.req.flash('info', 'El pass es incorrecto');
-
-                    this.index();
-
-                }
-
-            }
-        })
-    }
-    index()
-
-    {
-
-        let info = this.req.flash('info');
-
-        if (info == "")
-
-        {
-
-            console.log(" NO Existe info");
-
-            this.res.render('register',
-
-                {
-                    title: 'register',
-                    layout: 'layout2'
-                });
-
-        } else {
-
-            console.log("Existe info");
-            this.res.render('register',
-
-                {
-                    title: 'register',
-                    layout: 'layout2',
-                    info: info
-                });
-
-            info = "";
-
+    index() {
+        if (this.req.flash.error) {
+            console.log(this.req.flash.error);
+            this.res.render('register', { error: this.req.flash.error });
         }
-
+        this.res.render('register');
     }
 
-    register() {
-        // let a = this.req.body.username; 
-        //let b = password;
-        // let userModel = new UserModel();
-        //usermodel.findUser
-        //usermodel.findEmail
-        //usermodel.register(fullname, username, email, password, (info)=>{
 
-        //})
 
-        let username = this.req.body.username;
-        let password = this.req.body.password;
-        let fullName = this.req.body.fullName;
-        let email = this.req.body.email;
+    register() {;
+        // let username = this.req.body.username;
+        // let password = this.req.body.password;
+        // let fullName = this.req.body.fullName;
+        // let email = this.req.body.email;
         let userModel = new UserModel();
-        // userModel.findUser
-        console.log('Usuario ->' + fullName);
 
-        userModel.register(username, email, password, fullName, (info) => {
-            console.log(info)
-        })
+        let registerData = {...this.req.body };
 
 
-        this.res.redirect('/login');
+
+
+
+        userModel.getUserByEmailOrUsername(registerData.email, registerData.username)
+            .then((data) => {
+                if (data.length === 0) {
+                    let identService = new IdentService();
+                    registerData.hash = identService.getUUIDD(3, 4);
+                    let registerService = new RegisterService();
+                    registerData.password = registerService.encryptPass(registerData.password);
+                    userModel.insertUser(registerData);
+                    let emailService = new EmailService();
+                    emailService.sendRegisterEmail(registerData);
+                    this.res.redirect('/login');
+
+                } else {
+                    this.req.flash.error = "El username o el email ya esta en uso";
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
 }
+
 module.exports = registerController;

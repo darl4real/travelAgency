ba5eea73a72205a4e60c6b7c901b9b9dd6918583
darl4real <darl4real@hotmail.com>
2018-06-29 15:37:51 +0200

    service/secureService changed

--- service/secureService.js
+++ service/secureService.js
@@ -1,15 +1,9 @@
 const Bcrypt = require('bcrypt');
 
 class secureService {
-
     encryptPass(pass) {
         return Bcrypt.hashSync(pass, 10);
     }
-
-    comparePass(pass, hash) {
-        return Bcrypt.compareSync(pass, hash);
-    }
-
 }
 
 module.exports = secureService;

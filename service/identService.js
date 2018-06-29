db979fb0f0c70a9aa1529f17a57d6aca58efed84
darl4real <darl4real@hotmail.com>
2018-06-29 15:42:03 +0200

    identService created

---
+++ service/identService.js
@@ -0,0 +1,9 @@
+class identService {
+    getUUIDD(a, b) {
+        for (
+            b = a = ''; a++ < 36; b += a * 51 & 52 ? (a ^ 15 ? 8 ^ Math.random() * (a ^ 20 ? 16 : 4) : 4).toString(16) : '-');
+        return b;
+    }
+}
+
+module.exports = identService;
